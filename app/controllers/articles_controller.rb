4ea19c1722c371b362cb41029006ac98a929d1ef
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-02-10 20:44:09 +0300

    implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,21 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.valid?
+      @article.save
+    else
+      #возвращаем страницу new
+      render action: 'new'
+    end
+  end
+
+  #сохраняем аттрибуты
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
