9d08d599064f4d71a8690616b21fa7b5d41df8cc
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-02-10 21:04:38 +0300

    update articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       #возвращаем страницу new
       render action: 'new'
