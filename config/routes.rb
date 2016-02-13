2f8460666f5fc0fc69d5a0b40e5f599bd1fb00c0
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-02-13 22:30:24 +0300

    add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   #get 'contacts' => 'contacts#new'
 
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
