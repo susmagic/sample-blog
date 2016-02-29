788f53df529307aa43cae84f83d3bf8bf166e76c
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-02-29 22:45:50 +0300

    add sign_up method

---
+++ spec/support/session_helper.rb
@@ -0,0 +1,8 @@
+def sign_up
+  fill_in :user_email, with: 'user@example.com'
+  fill_in :user_username, with: 'like'
+  fill_in :user_password, with: 'kapusta123'
+  fill_in :user_password_confirmation, with: 'kapusta123'
+
+  click_button 'Sign up'
+end
