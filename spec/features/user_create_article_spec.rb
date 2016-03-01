75cc772ebc4db967455397f79ef61e4aeec1a681
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-03-01 22:08:29 +0300

    update i18n

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -8,6 +8,6 @@ feature 'Article creation' do
   scenario 'allows user to visit new article page' do
     visit new_article_path
 
-    expect(page).to have_content 'New article'
+    expect(page).to have_content I18n.t('contacts.new_article')
   end
 end
--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 feature 'Contact creation' do
+
   scenario 'allows access to contacts page' do
-    visit '/contacts'
+    visit contacts_path
 
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
 
   scenario 'allows a guest to create contact' do
-    visit '/contacts'
+    visit new_contacts_path
 
     fill_in :contact_email, with: 'user@example.com'
     fill_in :contact_message, with: 'test text'
 
     click_button 'Send Message'
 
-    expect(page).to have_content 'thanks'
+    expect(page).to have_content I18n.t('contacts.thanks')
   end
+
 end
